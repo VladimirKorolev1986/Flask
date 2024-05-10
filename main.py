c69aaf31de618c9bb8fbe8c96f909170dcf2eae7
VladimirKorolev <i@devkorolev.ru>
2024-05-10 21:13:32 +0300

    add static

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 menu = ['Установка', 'Первое приложение', 'Обратная связь']
@@ -7,13 +7,25 @@ menu = ['Установка', 'Первое приложение', 'Обратн
 @app.route('/index')
 @app.route('/')
 def index():
+    print(url_for('index'))
     return render_template('index.html', menu=menu)
 
 
 @app.route('/about')
 def about():
+    print(url_for('about'))
     return render_template('about.html', title='О сайте', menu=menu)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/profile/<username>')
+def profile(username, path):
+    return f'Пользователь: {username, path}'
+
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('about'))
+    print(url_for('profile', username='selfedu'))
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
