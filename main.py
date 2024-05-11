50f2646cd6b87fce07753c2cfbffef43ef9e904f
VladimirKorolev <i@devkorolev.ru>
2024-05-11 16:45:38 +0300

    add profile

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, request, flash, session, redirect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dfdsfsfdsfsdgtymklam2392jre2klm'
+
 menu = [{"name": "Установка", "url": "install-flask"},
         {"name": "Первое приложение", "url": "first-app"},
         {"name": "Обратная связь", "url": "contact"}]
@@ -28,10 +30,25 @@ def profile(username, path):
 def contact():
     if request.method == "POST":
         if len(request.form['username']) > 2:
-            flash('Сообщение отправлено')
+            flash('Сообщение отправлено', category='success')
         else:
-            flash('Ошибка отправки')
-    return render_template('contact.html', title='Обратная связь', menu=menu)
+            flash('Ошибка отправки', category='error')
+    return render_template('contact.html', title='Обратная связь', menu=menu), 404
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == 'selfedu' and request.form['psw'] == "123":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+    return render_template('login.html', title='Авторизация', menu=menu)
+
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template('page404.html', title='Страница не найдена', menu=menu)
 
 
 # with app.test_request_context():
